f07265625559b7afc1e5459deeacc388de629217
Ashish Mandal <ashishmandal999@gmail.com>
2023-02-16 11:04:47 +0530

    Prime Problem Code

---
+++ prime_problem.js
@@ -0,0 +1,16 @@
+
+
+let num = 7;
+let count=0;
+
+for(let i=1; i<=num; i++){
+    if(num%i === 2){
+        count
+    }
+}
+if(count==2){
+    console.log("Yes")
+}
+else{
+    console.log("No")
+}
